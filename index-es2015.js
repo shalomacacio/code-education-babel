cb99f6a33f80f594c0d6b71f97057f327cc4f804
Shalom Acacio <shalom.acacio@gmail.com>
2019-05-28 00:59:24 -0300

    primeiro commit

---
+++ index-es2015.js
@@ -0,0 +1,3 @@
+"use strict";
+
+var a = "\n\t\t<p>teste1</p>\n\t\t<p>teste2</p>\n\t\t<p>teste3</p>\n\t\t<p>teste4</p>\n\t\t";
---
+++ index.js
@@ -0,0 +1,6 @@
+var a = `
+		<p>teste1</p>
+		<p>teste2</p>
+		<p>teste3</p>
+		<p>teste4</p>
+		`
